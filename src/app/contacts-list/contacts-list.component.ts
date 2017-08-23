591c6f92331b213bdee328c24384eb168105cfdb
CHBaker <charlesprobaker@mail.com>
2017-08-23 15:38:14 -0500

    FEATURE: merge streams
    totally rad complex/simple merge of observable streams during search
    server call using several operators

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -2,6 +2,9 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/takeUntil';
 
 import { ContactsService } from './../contacts.service';
 import { Contact } from './../models/contact';
@@ -17,20 +20,24 @@ export class ContactsListComponent implements OnInit{
     contactList$: Observable<Array<Contact>>;
     private terms$ = new Subject<string>();
 
-    constructor(private contactsService: ContactsService) {
-        this.contactList$ = this.contactsService.getContacts()
-    }
+    constructor(private contactsService: ContactsService) {}
 
     trackContact(index: number, contact: Contact) {
         return contact.id;
     }
 
     ngOnInit() {
-        this.terms$.debounceTime(400).distinctUntilChanged()
-            .subscribe(
-                (term) => {
-                    this.contactList$ = this.contactsService.search(term);
-                }
-        );
+        let first$ = this.contactsService.getContacts();
+
+        let later$ = this.terms$
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .switchMap(term => this.contactsService.search(term));
+
+        this.contactList$ = later$.merge(first$.takeUntil(later$));
+
+        // first$ --X--|---
+        // later$ -----X---
+        // rslts$ --X--X--
     }
 }
